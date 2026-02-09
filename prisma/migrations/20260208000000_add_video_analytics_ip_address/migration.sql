0076648fcf3d96f39eb2b0f74b657d00e14b0d00
simbamcsimba <simbamcsimba@users.noreply.github.com>
2026-02-09 13:51:21 +1000

    Remove duplicate video analytics migration

--- prisma/migrations/20260208000000_add_video_analytics_ip_address/migration.sql
+++
@@ -1,5 +0,0 @@
--- Add viewer IP address to VideoAnalytics
-
-ALTER TABLE "VideoAnalytics" ADD COLUMN "ipAddress" TEXT;
-
-CREATE INDEX "VideoAnalytics_ipAddress_idx" ON "VideoAnalytics"("ipAddress");
